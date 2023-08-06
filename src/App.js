b0994d9b532bd1efd13960a5f609cac5253e33b4
Richa Yadav <richa.94yadav@gmail.com>
2023-08-06 12:17:47 +0530

    Sc

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const user = useSelector((data) => data.users);
   return (
     <div className="App">
-      <div>{JSON.stringify(user)}</div>
+      {/* <div>{JSON.stringify(user)}</div> */}
       {user.name.length <= 0 ? <Login /> : <Logout />}
     </div>
   );
